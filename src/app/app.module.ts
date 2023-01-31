6d9a1b5715e04d1e056cc63627689ff248eeaf60
Julia <113190885+xisanex@users.noreply.github.com>
2023-01-31 14:53:34 +0100

    css login refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
@@ -8,7 +9,7 @@ import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [AppComponent, MenuComponent, HomeComponent, LoginComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-
+import { trigger, state, style, animate, transition } from '@angular/animations';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  // animations: [trigger('openClose'), [state('open', style({})), state('closed',style({}))]],
 })
-export class LoginComponent {
-
-}
+export class LoginComponent {}

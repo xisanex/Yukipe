d83cfe20c9dd7a9b338084fb9f5869702a4824aa
Julia <113190885+xisanex@users.noreply.github.com>
2023-02-14 15:45:21 +0100

    products ,service and basic css

--- src/app/products/list-of-products.service.ts
+++ src/app/products/list-of-products.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
 
+export interface Products {
+  imagePath: string;
+  category: string;
+  brandName: string;
+  price: number;
+}
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ListOfProductsService {
-
-  constructor() { }
+  public products: Products[] = [
+    {
+      imagePath:
+        'https://contents.mediadecathlon.com/p2052233/k$97717df7a83d287ebc3b27225aed2d6d/sq/8602244.jpg?format=auto&f=800x0&quality=40',
+      category: 'Snowboard',
+      brandName: 'Deska',
+      price: 230,
+    },
+    {
+      imagePath:
+        'https://www.airtracks.de/media/image/31/ba/53/herren-damen-snowboard-high-m-hybrid-rocker-camber-zero-flat-freeride-freestyle-140-145-150-155-152-cm-highm1_600x600.jpg',
+      category: 'Snowboard',
+      brandName: 'Deska',
+      price: 260,
+    },
+  ];
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ListOfProductsService, Products } from './list-of-products.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  public products: Products[] = [];
 
+  constructor(public listOfProductsService: ListOfProductsService) {}
+
+  ngOnInit(): void {
+    this.products = this.listOfProductsService.products;
+    console.log(this.products);
+  }
 }

b80dd45d336d753bf52198f6bf033e9d9cd38475
Julia <113190885+xisanex@users.noreply.github.com>
2023-02-16 15:34:16 +0100

    css

--- src/app/products/list-of-products.service.ts
+++ src/app/products/list-of-products.service.ts
@@ -26,5 +26,19 @@ export class ListOfProductsService {
       brandName: 'Deska',
       price: 260,
     },
+    {
+      imagePath:
+        'https://contents.mediadecathlon.com/p2052233/k$97717df7a83d287ebc3b27225aed2d6d/sq/8602244.jpg?format=auto&f=800x0&quality=40',
+      category: 'Snowboard',
+      brandName: 'Deska',
+      price: 230,
+    },
+    {
+      imagePath:
+        'https://www.airtracks.de/media/image/31/ba/53/herren-damen-snowboard-high-m-hybrid-rocker-camber-zero-flat-freeride-freestyle-140-145-150-155-152-cm-highm1_600x600.jpg',
+      category: 'Snowboard',
+      brandName: 'Deska',
+      price: 260,
+    },
   ];
 }
